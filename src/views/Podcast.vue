4bea01cf4e5ee9b824f5d0d02815ef5329cc07ef
javier <ertoyos@gmail.com>
2023-09-12 10:14:57 +0200

    add

--- src/views/Podcast.vue
+++ src/views/Podcast.vue
@@ -4,7 +4,7 @@ import { ref, onBeforeMount } from "vue";
 import ApiRepository from '../assets/ApiRepositories/ApiRepository'
 
 import HeadOne from "../components/HeadOne.vue";
-import ItemListEpisodesPodcast from "../components/ItemListEpidodesPopdcast.vue"
+import ItemListEpisodesPodcast from "../components/ItemListEpidodesPodcast.vue"
 
 const props = defineProps({
   id: {
