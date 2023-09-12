cd0993b6b82e60386c3991799e0fce937fbac724
javier <ertoyos@gmail.com>
2023-09-12 13:01:25 +0200

    add 2 view

--- src/views/Podcast.vue
+++ src/views/Podcast.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { ref, onBeforeMount } from "vue";
-
 import ApiRepository from '../assets/ApiRepositories/ApiRepository'
-
 import HeadOne from "../components/HeadOne.vue";
 import ItemListEpisodesPodcast from "../components/ItemListEpidodesPodcast.vue"
 
@@ -17,7 +15,18 @@ const api = repository.chooseApi();
 
 let podcast = ref([]);
 onBeforeMount(async () => {
-  podcast.value = await api.getAll(788236947);
+  const storedPodcast = localStorage.getItem('podcast');
+  const storedTime = localStorage.getItem('storedTime');
+  const currentTime = new Date().getTime();
+  const oneDay = 24 * 60 * 60 * 1000; 
+
+  if (storedPodcast && storedTime && currentTime - storedTime < oneDay) {
+    podcast.value = JSON.parse(storedPodcast);
+  } else {
+    podcast.value = await api.getAll(788236947);
+    localStorage.setItem('podcast', JSON.stringify(podcast.value));
+    localStorage.setItem('storedTime', currentTime.toString());
+  }
 })
 
 </script>
@@ -28,17 +37,17 @@ onBeforeMount(async () => {
     <div class="podcast">
       <div class="aboutPodcast">
         <figure>
-          <img src="" alt="" />
+          <img v-bind:src="podcast.results[0].artworkUrl100" alt="" />
         </figure>
         <div class="infoPodcast">
           <h2>{{ podcast.results[0].collectionName }}</h2>
           <p class="author">{{ podcast.results[0].artistName }}</p>
           <p><strong>Description</strong></p>
-          <p class="description">{{ podcast.results[0].trackViewUrl }}</p>
+          <p class="description" style="overflow: auto; text-overflow: ellipsis; white-space: nowrap;">{{ podcast.results[0].trackViewUrl }}</p>
         </div>
       </div>
       <div class="epidodesPodcast">
-        <p class="totalEpisodesPodcast">Episodios:</p>
+        <p class="totalEpisodesPodcast">Episodios: {{ podcast.results[0].trackCount }}</p>
         <div class="listEpidodesPodcast">
           <table>
             <thead>
@@ -49,12 +58,7 @@ onBeforeMount(async () => {
               </tr>
             </thead>
             <tbody>
-              <ItemListEpisodesPodcast></ItemListEpisodesPodcast>
-              <ItemListEpisodesPodcast></ItemListEpisodesPodcast>
-              <ItemListEpisodesPodcast></ItemListEpisodesPodcast>
-              <ItemListEpisodesPodcast></ItemListEpisodesPodcast>
-              <ItemListEpisodesPodcast></ItemListEpisodesPodcast>
-              <ItemListEpisodesPodcast></ItemListEpisodesPodcast>
+              <ItemListEpisodesPodcast v-for="(episode, index) in podcast.results" :key="index" :episode="episode.data"></ItemListEpisodesPodcast>
             </tbody>
           </table>
         </div>
@@ -62,7 +66,6 @@ onBeforeMount(async () => {
     </div>
   </main>
 </template>
-
 <style lang="scss" scoped>
 @import "../assets/scss/variables.scss";
 
@@ -74,22 +77,29 @@ main {
     gap: 20px;
 
     .aboutPodcast {
-      width: 100%;
-      min-width: 190px;
-      padding: 15px;
-      border: 1px solid $GrayMid;
-      border-radius: 5px;
-      figure {
-        width: 100%;
-        aspect-ratio: 1/1;
-        object-fit: cover;
-        padding-bottom: 10px;
-        border-bottom: 1px solid $GrayMid;
-        margin-bottom: 10px;
-      }
-      h2 {
-        font-size: 20px;
-      }
+  width: 100%;
+  min-width: 190px;
+  padding: 15px;
+  border: 1px solid $GrayMid;
+  border-radius: 5px;
+  figure {
+    width: 100%;
+    aspect-ratio: 1/1;
+    padding-bottom: 10px;
+    border-bottom: 1px solid $GrayMid;
+    margin-bottom: 10px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    img {
+      object-fit: cover;
+      min-width: 100%;
+      min-height: 100%;
+    }
+  }
+}
       .infoPodcast {
         .author {
           font-style: oblique;
@@ -143,5 +153,4 @@ main {
       }
     }
   }
-}
 </style>
